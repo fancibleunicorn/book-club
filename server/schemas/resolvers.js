602508fcb35919e0f79368c52c630b78b7331046
Adam Crandall <adamcrandall91@gmail.com>
2021-03-13 18:01:17 -0500

    created removeBook funcationality, finished with backend

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -57,6 +57,21 @@ const resolvers = {
                 }
                 throw new AuthenticationError('You need to be logged in!');
                 
+            },
+            removeBook: async (parent, { bookId }, context) => {
+                if (context.user) {
+
+                    const updatedUser = await User.findByIdAndUpdate(
+                        { _id: context.user._id },
+                        { $pull: { savedBooks: {bookId: bookId }} },
+                        { new: true, runValidators: true }
+                    )
+                    ;
+
+                    return updatedUser;
+                }
+                throw new AuthenticationError('You need to be logged in!');
+                
             }
     }
       
